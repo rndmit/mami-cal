093b1a8549afd9a120b5806b5ad149759b544564
Tim Scobcov <scobcov@icloud.com>
2019-03-20 17:31:02 +0300

    [*] Some parts refactored and commented

--- app/ical.py
+++ app/ical.py
@@ -2,33 +2,48 @@ import arrow
 from datetime import timedelta
 from ics import Calendar, Event
 
-import parser
-from consts import lesson_time
 
 TIMESTAMP_FORMAT = 'YYYY-MM-DD:HH-mm'
 TIMEZONE = 'Europe/Moscow'
+LESSON_TIME = {
+    '1': '09-00',
+    '2': '10-40',
+    '3': '12-20',
+    '4': '14-30',
+    '5': '16-10',
+    '6': '17-50',
+    '7': '19-30',
+}
 
 def build(data: dict):
-    calendar = Calendar()
+    '''Builds an calendar from the dictionary
 
+        Args:
+            data (dict) - recieved from rasp.dmami.ru JSON
+        Returns: ics.Calendar
+    '''
+
+    calendar = Calendar()
     for day in data.keys():
         for lesson in data[day].keys():
             for item in data[day][lesson]:
-                print(item['subject']) #Just for debugging
 
+                # Если аудиторий несколько - собираем их в одну строку
                 location = str()
                 if len(item['auditories']) == 1:
                     location = item['auditories'][0]['title']
                 else:
                     for i in item['auditories']:
                         location += (i['title'])
-                
-                df = arrow.get(item['date_from'] + ':' + lesson_time[lesson], TIMESTAMP_FORMAT).replace(days=(int(day) - 1))
-                print(df)
-                dt = arrow.get(item['date_to'] + ':' + lesson_time[lesson], TIMESTAMP_FORMAT)
 
+                # Формируем таймстэмпы начала и конца текущей дисциплины с учетом времени начала пары
+                df = arrow.get(item['date_from'] + ':' + LESSON_TIME[lesson], TIMESTAMP_FORMAT).replace(days=(int(day) - 1))
+                print(df)
+                dt = arrow.get(item['date_to'] + ':' + LESSON_TIME[lesson], TIMESTAMP_FORMAT)
+
+                # Проходим по всем числам от начала до конца с интервалом в неделю
                 for r in arrow.Arrow.range('week', df, dt):
-                    print(r.tzinfo)
+                    # Формируем событие и сразу добавляем его в календарь
                     calendar.events.add(Event(
                         name=item['subject'],
                         begin=r.replace(tzinfo=TIMEZONE),
@@ -36,7 +51,15 @@ def build(data: dict):
                         location=location,
                         description='Преподаватель: ' + item['teacher']
                     ))
-    open('caricular.ics', 'w').writelines(calendar)
 
-if __name__ == '__main__':
-    build(parser.get_data('181-362'))
+    return calendar
+
+def save_to_ics(calendar: Calendar, filename: str):
+    '''Saves calendar to ics-file with the specified name
+
+        Args:
+            calendar: ics.Calendar - calendar object which should be saved to a file
+            filename: str
+        Returns: None
+    '''
+    open('caricular.ics', 'w').writelines(calendar)
--- app/parser.py
+++ app/parser.py
@@ -5,11 +5,15 @@ import datetime
 
 API_DOMAIN = 'https://rasp.dmami.ru/'
 
-def get_bpc():
-    responce = requests.get(API_DOMAIN).text
-    return re.search(r'(?<=bpc\=).*(?=\;Path)', responce).group(0)
 
 def get_data(group: str):
+    '''Gets the schedule from the server
+
+        Args: 
+            group: str - defines the student group for which to get the schedule
+        Returns: dict - represents recieved JSON
+    '''
+
     cookies = {
         'bpc': get_bpc(),
         'group': group
@@ -17,9 +21,14 @@ def get_data(group: str):
     headers = {'referer': API_DOMAIN }
     gateway = f'{API_DOMAIN}site/group?group={group}&session=0'
     return requests.post(gateway, cookies=cookies, headers=headers).json()['grid']
-    
 
-if __name__ == '__main__':
-    data = get_data('181-362')
-    print(data)
-    print(data.keys())
+
+def get_bpc():
+    '''Sends n̶u̶d̶e̶s̶ an empty request to receive cookies
+
+        Args: none
+        Returns: str - bpc string
+    '''
+
+    responce = requests.get(API_DOMAIN).text
+    return re.search(r'(?<=bpc\=).*(?=\;Path)', responce).group(0)
